f99c6de2151487f06f2091fa2c7a4096bc7bfd74
jacksonPrimo <jackson.primodev@gmail.com>
2026-02-05 21:37:36 -0300

    add pack model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_30_001217) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_04_000158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "packs", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_packs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +31,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_30_001217) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "packs", "users"
 end
