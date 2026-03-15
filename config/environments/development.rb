60c7182d5b92d7b49a7417a1d06136c50ec30a23
jacksonPrimo <jackson.primodev@gmail.com.br>
2026-03-15 16:18:40 -0300

    config action_mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,15 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true 
+  config.action_mailer.smtp_settings = {
+    address:              ENV.fetch('SMTP_ADDRESS', 'smtp.gmail.com'),
+    port:                 ENV.fetch('SMTP_PORT', 587).to_i,
+    domain:               ENV.fetch('SMTP_DOMAIN', 'gmail.com'),
+    user_name:            ENV.fetch('SMTP_USERNAME', nil),
+    password:             ENV.fetch('SMTP_PASSWORD', nil),
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,16 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.default_url_options = { host: "exemplo.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true 
+  config.action_mailer.smtp_settings = {
+    address:              ENV.fetch('SMTP_ADDRESS', 'smtp.gmail.com'),
+    port:                 ENV.fetch('SMTP_PORT', 587).to_i,
+    domain:               ENV.fetch('SMTP_DOMAIN', 'gmail.com'),
+    user_name:            ENV.fetch('SMTP_USERNAME', nil),
+    password:             ENV.fetch('SMTP_PASSWORD', nil),
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
 end
