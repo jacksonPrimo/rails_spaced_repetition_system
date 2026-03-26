30e71821136680f6a933fbe97fa7e95f4fc9fe4f
jacksonPrimo <jackson.primodev@gmail.com.br>
2026-03-25 21:34:25 -0300

    add variant thumb to avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_one_attached :avatar
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
